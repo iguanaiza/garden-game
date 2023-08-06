7f5ac42aec3fcce942d09adb34d5958d48f5aed3
Iza <mrs.master.of.disaster@gmail.com>
2023-08-06 16:16:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,105 @@
-//consts
-const infoText = document.querySelector("#info-text");
-const infoButton = document.querySelector("#info");
-const navLeft = document.querySelector("#left");
-const navRight = document.querySelector("#right");
-const navUp = document.querySelector("#up");
-const navDown = document.querySelector("#down");
-const plants = document.querySelectorAll(".plant");
-const water = document.querySelector("#water");
-const fertilizer = document.querySelector("#fertilizer");
-const heal = document.querySelector("#heal");
-const cut = document.querySelector("#cut");
+//=============VARIABLES=====================================
+const garden = document.querySelector('#garden'); //garden div
+const infoText = document.querySelector("#info-text"); //game info div
+const infoButton = document.querySelector("#info"); //button with info about game
+const navLeft = document.querySelector("#left"); //button with arrow left - navigation
+const navRight = document.querySelector("#right"); //button with arrow right - navigation
+const water = document.querySelector("#water"); //button with drop - to water the plant
+const fertilizer = document.querySelector("#fertilizer"); //green button - to fertilize the plant
+const heal = document.querySelector("#heal"); //button with ambulance - to heal the plant
+const cut = document.querySelector("#cut"); //button with axe - to cut the plant
+const plant1 = {id: "plant1", watering: 0, size: 1}; //1st plant
 
-//info
+let plantsStorage = [plant1]; //array with all plants objects - 1st flower is already added
+let selectedPlant = plantsStorage[0]; //by deafult 1st plant is selected
+let newID = 2; //starting new IDs from 2
+let currentPlant = 0; //current plant is 0 in array
+//-----------------------------------------------------------
+
+//=============INFO ABOUT GAME - UNHIDE/HIDE ON CLICK========
 infoButton.addEventListener("click", () => 
 {
-    infoText.classList.toggle("hidden");
+  infoText.classList.toggle("hidden"); //toggles class 'hidden'
 });
+//-----------------------------------------------------------
 
-//navigation
-let currentPlant = 1;
-let selectedPlant = document.querySelectorAll(".selected")[0];
+//=============NAVIGATION====================================
+function changePlant(plantNum) { //changing current plant selection
+  
+  let clear = document.querySelector(".selected"); //selects all plants with class 'selected'
+  let plant = document.getElementById(plantsStorage[plantNum].id); //selects plant by id starting with 'plant' and current number (like #plant1)
+  
+  clear.classList.remove("selected"); //clears class from all plants
+  plant.classList.add("selected"); //adds class to current plant
 
-function changePlant(plantNum) {
-    if (plantNum < 1 || plantNum > plants.length) {
-      return;
-    }
-  
-    const clear = document.querySelector(".selected");
-    const plant = document.querySelector("#plant" + plantNum);
-  
-    clear.classList.remove("selected");
-    plant.classList.add("selected");
-    selectedPlant = plant;
+  selectedPlant = plantsStorage[plantNum]; //reassigns current selection
 }
 
-navRight.addEventListener("click", () => {
-    let newPlant = currentPlant + 1;
+//behaviour on button 'right' click
+navRight.addEventListener("click", () => { 
+    let nextPlant = currentPlant + 1; //takes current selections and adds 1 to move to right
 
-    if (newPlant > plants.length) {
-      newPlant  = 1;
+    if (nextPlant > plantsStorage.length-1) { //resets to 1st plant when moves from the last one to next one
+      nextPlant = 0;
     }
 
-    changePlant(newPlant);
-    currentPlant = newPlant;
+    changePlant(nextPlant); //uses the changePlant func
+    currentPlant = nextPlant; //reassigns current selection
 });
 
+//behaviour on button 'left' click
 navLeft.addEventListener("click", () => {
-    let newPlant = currentPlant - 1;
+    let previousPlant = currentPlant - 1; //takes current selections and subtracks 1 to move to left
 
-    if (newPlant < 1) {
-      newPlant  = plants.length;
+    if (previousPlant < 0) { //resets to last plant when moves from the first one to previous one
+      previousPlant = plantsStorage.length-1;
     }
 
-    changePlant(newPlant);
-    currentPlant = newPlant;
+    changePlant(previousPlant); //uses the changePlant func
+    currentPlant = previousPlant; //reassigns current selection
 });
+//-----------------------------------------------------------
 
-//water
-function changeSize(size) {
-  let sizes = ["x-small", "small", "medium", "large", "x-large", "xx-large"];
+//=============WATERING THE PLANT============================
+function waterPlant() {
+  let newSize = selectedPlant.size + 0.5; //new size value - 0,5 bigger than default
+  let plantID = document.getElementById(selectedPlant.id); //ID of selected plant
 
-  selectedPlant.style.fontSize = sizes[size];
-/*  switch(size) {
-    case 1:
-      selectedPlant.style.fontSize = "small";
-      break;
-    case 2:
-      selectedPlant.style.fontSize = "medium";
-      break;
-    case 3:
-      selectedPlant.style.fontSize = "large";
-      break;
-    case 4:
-      selectedPlant.style.fontSize = "x-large";
-      break;
-    case 5:
-      selectedPlant.style.fontSize = "xx-large";
-      break;
-  }*/
-}
-
-let currentSize = 0
-
-water.addEventListener("click", () => {
-  let newSize = currentSize+1;
-
-  if (newSize > 5) {
-    selectedPlant.innerHTML="💀";
+  if (newSize <= 3) { //max size is 3rem which is 5 waters
+    plantID.style = `font-size: ${selectedPlant.size}rem`;
+    selectedPlant.size = selectedPlant.size + 0.5;
+    currentSize = newSize;
   }
 
-  changeSize(newSize);
-  currentSize = newSize;
-});
-//fertilizer
+  else if (newSize > 3) {//if overwatered then it dies
+    plantID.innerHTML="💀"; //reassigns flower icon to skull
+  }
 
+  else return;
+}
+
+water.addEventListener("click", waterPlant); //action on water button click
+//-----------------------------------------------------------
+
+//=============FERTILIZER====================================
+//fertilize
+function fertPlant() {
+  let newFlower = document.createElement('div'); //new flower div
+
+  if (plantsStorage.length > 99){ //if more than 100 flowers - we cant add more
+    return;
+  }
+
+  newFlower.id = "plant"+newID; //new flower ID assign
+  newFlower.classList.add("plant"); //new flower class assign
+  newFlower.innerText = `🌷`; //new flower emoji assign
+  garden.appendChild(newFlower); //new flower added to garden
+
+  plantsStorage.push({id: "plant"+newID, watering: 0, size: 1}); //add new flower object to list
+  newID++; //increase newID value
+}
+
+fertilizer.addEventListener("click", fertPlant);//action on fertilizer button click
 //heal
 
 //cut
